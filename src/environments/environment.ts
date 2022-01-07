d1cd390a731c7f16ed6590ffe57de26038dec807
GURKAN-CAKIR <20201129078@ogr.akdeniz.edu.tr>
2022-01-07 15:58:38 +0300

    Emlak

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig : {
+  apiKey: "AIzaSyDHZIvryu0Dizd00IyVY9SRYDTQhNWjME4",
+  authDomain: "emlak-56ee8.firebaseapp.com",
+  databaseURL: "https://emlak-56ee8-default-rtdb.firebaseio.com",
+  projectId: "emlak-56ee8",
+  storageBucket: "emlak-56ee8.appspot.com",
+  messagingSenderId: "986888417903",
+  appId: "1:986888417903:web:036fc47f0f25578709139a",
+  measurementId: "G-PG5E0VJYG2"
+}
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
